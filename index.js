75dc37b017529206f2e097eee66b55532b166768
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-08-29 18:05:56 +0100

    javascript functionality and responsive styling

--- index.js
+++ index.js
@@ -5,9 +5,48 @@
   - The email address is not formatted correctly */
 
   const emailInput = document.getElementById('email')
+  const emailError = document.getElementById('emailError')
+  const successMsg = document.getElementById('successMessage')
+ const form = document.querySelector('form')
+ const submitBtn = document.querySelector('.signup-submit')
 
   emailInput.addEventListener('blur', validateEmail)
 
+function isEmailValid(emailInput){
+    const validEmail = /^[\w\d.%+-_]+@+[\w\d.%+-_]+\.+[\w]{2,}$/g
+    return validEmail.test(emailInput)
+}
+
   function validateEmail(event) {
-    console.log(event.target.value)
+    const input = event.target.value
+    const isValidEmail=isEmailValid(input)
+    if (!isValidEmail) {
+        emailError.innerText = "Please enter a valid email"
+        emailInput.classList.add('red-border')
+        emailInput.classList.remove('green-border')
+
+    }
+    else {
+        emailInput.classList.remove('red-border')
+        emailInput.classList.add('green-border')
+        emailError.innerText = ""
+    }
+}
+form.addEventListener('submit', handleSubmit)
+submitBtn.addEventListener('click', handleSubmit)
+
+  function handleSubmit(event) {
+
+const inputValue = emailInput.value
+const inputValueIsValid = isEmailValid(inputValue)
+if (!inputValueIsValid) {
+    event.preventDefault()
+    successMsg.innerText=""
+}
+else {
+    event.preventDefault()
+    successMsg.innerText="Sign-up success!"
+    form.classList.add("hidden")
+}
+
   }
